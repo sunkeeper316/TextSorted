3cc45a0e35cffecb313e9843bc344c535ee96a07
huangdesang <sunkeeper316@gmail.com>
2021-05-21 08:58:17 +0800

    init

--- TextSorted/ViewController.swift
+++ TextSorted/ViewController.swift
@@ -1,16 +1,20 @@
-//
-//  ViewController.swift
-//  TextSorted
-//
-//  Created by sunkeeper on 2021/5/20.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var strArray = ["三","c","ㄧ","G","h","一七二","五","ㄠ","一四","一二" , "A" ,"a" ,"ㄕ","C"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        let a = "1"
+        let b = "b"
+        a.compare(b , options: .numeric)
+        strArray.sort { strA, strB in
+            return strA.localizedCompare(strB) == .orderedAscending
+            
+        }
+        print(strArray)
         // Do any additional setup after loading the view.
     }
 
